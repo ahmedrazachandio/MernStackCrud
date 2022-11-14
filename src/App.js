dfdc02fb94042aa931e9786c382da1c84de95518
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-11-14 16:16:16 +0500

    errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import { Route, BrowserRouter,Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import Register from './components/Register';
+import React from 'react';
 
 function App() {
   return (
     <>
     <Navbar />
-    <Home />
+
+
+    <BrowserRouter>
+      <div className="app">
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register/>} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+
+
+
+
+
+   
     <Footer />
     </>
   );
