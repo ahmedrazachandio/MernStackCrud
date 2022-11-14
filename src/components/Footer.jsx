bcd9d017e5f9190f6281026c58d21eb89cbe6381
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-11-14 14:37:10 +0500

    read section complete

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="" style={{ marginTop :"360px"}}>
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2022 Copyright:
+        <a className="text-black" href="https://mdbootstrap.com/">
+          ahmedrazachandio.github.io
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Home.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mt-5">
+      <div className="container">
+        <div className="add_btn mt-2">
+          <button className="btn btn-primary">Add Data</button>
+        </div>
+        <table class="table mt-3">
+          <thead>
+            <tr className="table-dark">
+              <th scope="col">Id</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Job</th>
+              <th scope="col">Number</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Ahmed Raza</td>
+              <td>ahmedraza</td>
+              <td>ahmadraza@art.com</td>
+              <td>92+3222665676</td>
+              <td className="d-flex justify-content-between">
+                <button className="btn btn-success"><i class="fa-brands fa-readme"></i></button>
+                <button className="btn btn-primary"><i class="fa-regular fa-pen-to-square"></i></button>
+                <button className="btn btn-danger"><i class="fa-sharp fa-solid fa-trash"></i></button>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Ahmed Raza</td>
+              <td>ahmedraza</td>
+              <td>ahmadraza@art.com</td>
+              <td>92+3222665676</td>
+              <td className="d-flex justify-content-between">
+                <button className="btn btn-success"><i class="fa-brands fa-readme"></i></button>
+                <button className="btn btn-primary"><i class="fa-regular fa-pen-to-square"></i></button>
+                <button className="btn btn-danger"><i class="fa-sharp fa-solid fa-trash"></i></button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
